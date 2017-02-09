f468c259e8909eb529f893922d9d5bd0559d2e43
jreplogle Replogle <jreplogle Replogle>
2017-02-08 22:35:37 -0600

    Complete basic media viewer.

---
+++ CSharpBasicMediaPlayer/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CSharpBasicMediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(146, 329);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(83, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(142, 36);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(612, 376);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
+            this.fileToolStripMenuItem.Text = "EXECUTION MENU";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(829, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 424);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ CSharpBasicMediaPlayer/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpBasicMediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.IWMPPlaylist playlist;
+        public Form1()
+        {
+            InitializeComponent();
+            playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            //Multiselect ~ If the user wants to play an entire set/list/album.
+            od.Multiselect = true;
+            //Filter types of media files:
+            od.Filter = "(mp3, wav, mp4, mov, wmv, 3gp, mpg, flv) | *.mp3; *.wav; *.mp4; *.mov; *.wmv; *.3pg; *.mpg; *.flv | all files | *.*";
+            if(od.ShowDialog() == DialogResult.OK)
+            {
+                if(listBox1.Items.Count > 0)
+                {
+                    listBox1.Items.Clear();
+                    axWindowsMediaPlayer1.currentPlaylist.clear();
+                }
+                for(int i = 0; i < od.SafeFileNames.Length; i++)
+                {
+                    listBox1.Items.Add(od.SafeFileNames[i]);
+                    WMPLib.IWMPMedia media = axWindowsMediaPlayer1.newMedia(od.FileNames[i]);
+                    playlist.appendItem(media);
+                }
+            }
+            axWindowsMediaPlayer1.currentPlaylist = playlist;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        //Listbox ~ Visible meida file q.
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox1.Items.Count > 0)
+            {
+                //Create new media from listbox.
+                WMPLib.IWMPMedia media = axWindowsMediaPlayer1.currentPlaylist.get_Item(listBox1.SelectedIndex);
+                //Play media.
+                axWindowsMediaPlayer1.Ctlcontrols.playItem(media);
+            }
+        }
+
+        //(+)Button functionality:  Allows user to Add songs to Listbox via local files.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            //Multiselect ~ If the user wants to play an entire set/list/album.
+            od.Multiselect = true;
+            od.Filter = "(mp3, wav, mp4, mov, wmv, 3gp, mpg, flv) | *.mp3; *.wav; *.mp4; *.mov; *.wmv; *.3pg; *.mpg; *.flv | all files | *.*";
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                for (int i = 0; i < od.SafeFileNames.Length; i++)
+                {
+                    listBox1.Items.Add(od.SafeFileNames[i]);
+                    WMPLib.IWMPMedia media = axWindowsMediaPlayer1.newMedia(od.FileNames[i]);
+                    axWindowsMediaPlayer1.currentPlaylist.appendItem(media);
+                }
+            }
+        }
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (axWindowsMediaPlayer1.currentMedia != null || listBox1.Items.Count <= 0)
+                return;
+            for (int i = 0; i < axWindowsMediaPlayer1.currentPlaylist.count; i++)
+            {
+                if (axWindowsMediaPlayer1.currentMedia.get_isIdentical(axWindowsMediaPlayer1.currentPlaylist.Item[i]))
+                    listBox1.SelectedIndex = i;
+            }
+        }
+
+        //(-)Remove button method.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Stored selected itme from Listbox.
+            int selected = listBox1.SelectedIndex;
+            //Get media.
+            WMPLib.IWMPMedia media = axWindowsMediaPlayer1.currentPlaylist.get_Item(selected);
+            //Remove item.
+            axWindowsMediaPlayer1.currentPlaylist.removeItem(media);
+            //Stop the player.
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            //Remove selected item.
+            listBox1.Items.RemoveAt(selected);
+        }
+    }
+}
